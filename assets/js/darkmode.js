8b20fa32019c80e56df78df82d97a811510ce8ae
ShengGao-wharton <shenggao@wharton.upenn.edu>
2025-04-13 01:08:55 -0400

    change button

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,38 +1,3 @@
-// (function () {
-//   function enableDark() {
-//     document.documentElement.classList.add("dark");
-//     localStorage.setItem("theme", "dark");
-//   }
-
-//   function disableDark() {
-//     document.documentElement.classList.remove("dark");
-//     localStorage.setItem("theme", "light");
-//   }
-
-//   function toggleDarkMode() {
-//     if (document.documentElement.classList.contains("dark")) {
-//       disableDark();
-//     } else {
-//       enableDark();
-//     }
-//   }
-
-//   function setupButton() {
-//     const toggle = document.createElement("button");
-//     toggle.innerHTML = "🌓";
-//     toggle.id = "dark-toggle";
-//     toggle.title = "Toggle dark mode";
-//     document.body.appendChild(toggle);
-//     toggle.addEventListener("click", toggleDarkMode);
-//   }
-
-//   document.addEventListener("DOMContentLoaded", function () {
-//     if (localStorage.getItem("theme") === "dark") {
-//       enableDark();
-//     }
-//     setupButton();
-//   });
-// })();
 (function () {
   function enableDark() {
     document.documentElement.classList.add("dark");
@@ -68,4 +33,39 @@
     setupButton();
   });
 })();
+// (function () {
+//   function enableDark() {
+//     document.documentElement.classList.add("dark");
+//     localStorage.setItem("theme", "dark");
+//   }
+
+//   function disableDark() {
+//     document.documentElement.classList.remove("dark");
+//     localStorage.setItem("theme", "light");
+//   }
+
+//   function toggleDarkMode() {
+//     if (document.documentElement.classList.contains("dark")) {
+//       disableDark();
+//     } else {
+//       enableDark();
+//     }
+//   }
+
+//   function setupButton() {
+//     const toggle = document.createElement("button");
+//     toggle.innerHTML = "🌓";
+//     toggle.id = "dark-toggle";
+//     toggle.title = "Toggle dark mode";
+//     document.body.appendChild(toggle);
+//     toggle.addEventListener("click", toggleDarkMode);
+//   }
+
+//   document.addEventListener("DOMContentLoaded", function () {
+//     if (localStorage.getItem("theme") === "dark") {
+//       enableDark();
+//     }
+//     setupButton();
+//   });
+// })();
 
